45ac43b95caa845d4e851ba8e53365d6810c6f8e
Kausalprasad <95764489+Kausalprasad@users.noreply.github.com>
2024-08-02 23:45:37 -0700

    Add files via upload

---
+++ viedo55.js
@@ -0,0 +1,35 @@
+// var a=87;
+// // console.log("hello")
+// console.log(a);
+// var b=76;
+// // console.log(a+b)
+// // var d="kaushal"
+// // console.log(typeof a, typeof b , typeof d);
+// // const a1=43;
+// // console.log(a1)
+// {
+//     let a=870;
+//     console.log(a)
+// }
+// console.log(a);
+
+let a=98;
+let b=78.8;
+let f=true;
+let c="Kaushal";
+let d=null;
+const g=87;
+let e=undefined;
+  console.log(typeof a, typeof b , typeof c, typeof d, typeof e ,typeof f,typeof g);
+
+  let o={
+    "name":"kaushal",
+    "is_hansome":true,
+    "age":14,
+     "id":6544,
+     "salary":7000,
+
+  }
+  o.salary=87777;
+  console.log(o);
+  console.log(o)
